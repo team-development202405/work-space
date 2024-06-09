41b5959e3ad61c5de283f389dfa407cf3285c336
asahihayate <ha.asahi.business@gmail.com>
2024-06-09 21:25:21 +0900

    マップ追加

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// mvのスライダーアニメーションの記述
+
+$(function () {
+  $(".mv__slider").slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 3000, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 1500, // スライドが流れる速度を設定
+    cssEase: "ease-in-out", // スライドの流れ方を等速に設定
+    slidesToShow: 1, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
+        },
+      },
+    ],
+  });
+});
